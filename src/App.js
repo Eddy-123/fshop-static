0f3a0fb8457f1bcce98ee9e5a7bac0624478823e
Eddy-123 <57044627+Eddy-123@users.noreply.github.com>
2021-08-08 15:59:41 +0100

    header and footer

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Ferrew Shop</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="grid-container">
+      <header className="header">
+        <div className="brand">
+          <a href="index.html">
+            <img src="./images/logo.png" />
+          </a>
+        </div>
+        <div className="header-links">
+          <a href="cart.html">Panier</a>
+          <a href="signin.html">Connexion</a>
+        </div>
+      </header>
+      <main className="main">
+        <ul>
+          <li>Ensemble des produits</li>
+          <li>Quelques produits</li>
+        </ul>
+      </main>
+      <footer className="footer">All right reserved.</footer>
     </div>
   );
 }
